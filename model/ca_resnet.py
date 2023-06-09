8434f2572c60a22540851430e5814a8fdc995242
LiHuan <784353397@qq.com>
2023-06-09 15:36:41 +0800

    利用resnet源代码复现模型

--- model/ca_resnet.py
+++ model/ca_resnet.py
@@ -1,52 +1,46 @@
+import torch.nn as nn
 import torch
-from torch import nn
-import torchvision
-class h_sigmoid(nn.Module):
-    def __init__(self, inplace=True):
-        super(h_sigmoid, self).__init__()
-        self.relu = nn.ReLU6(inplace=inplace)
+
+# 残差层 18和34层数
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_channel, out_channel, stride=1, downsample=None, **kwargs):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=in_channel, out_channels=out_channel,
+                               kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channel)
+        self.relu = nn.ReLU()
+        self.conv2 = nn.Conv2d(in_channels=out_channel, out_channels=out_channel,
+                               kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channel)
+        self.downsample = downsample
 
     def forward(self, x):
-        return self.relu(x + 3) / 6
+        identity = x
+        if self.downsample is not None:
+            identity = self.downsample(x)
 
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
 
-class h_swish(nn.Module):
-    def __init__(self, inplace=True):
-        super(h_swish, self).__init__()
-        self.sigmoid = h_sigmoid(inplace=inplace)
+        out = self.conv2(out)
+        out = self.bn2(out)
 
-    def forward(self, x):
-        return x * self.sigmoid(x)
+        out += identity
+        out = self.relu(out)
 
-
-class CoordAttention(nn.Module):
-
-    def __init__(self, in_channels, out_channels, reduction=32):
-        super(CoordAttention, self).__init__()
-        self.pool_w, self.pool_h = nn.AdaptiveAvgPool2d((1, None)), nn.AdaptiveAvgPool2d((None, 1))
-        temp_c = max(8, in_channels // reduction)
-        self.conv1 = nn.Conv2d(in_channels, temp_c, kernel_size=1, stride=1, padding=0)
-
-        self.bn1 = nn.BatchNorm2d(temp_c)
-        self.act1 = h_swish()
-
-        self.conv2 = nn.Conv2d(temp_c, out_channels, kernel_size=1, stride=1, padding=0)
-        self.conv3 = nn.Conv2d(temp_c, out_channels, kernel_size=1, stride=1, padding=0)
-
-    def forward(self, x):
-        short = x
-        n, c, H, W = x.shape
-        x_h, x_w = self.pool_h(x), self.pool_w(x).permute(0, 1, 3, 2)
-        x_cat = torch.cat([x_h, x_w], dim=2)
-        out = self.act1(self.bn1(self.conv1(x_cat)))
-        x_h, x_w = torch.split(out, [H, W], dim=2)
-        x_w = x_w.permute(0, 1, 3, 2)
-        out_h = torch.sigmoid(self.conv2(x_h))
-        out_w = torch.sigmoid(self.conv3(x_w))
-        return short * out_w * out_h
+        return out
 
 # 残差层数 50和101
 class Bottleneck(nn.Module):
+    """
+    注意：原论文中，在虚线残差结构的主分支上，第一个1x1卷积层的步距是2，第二个3x3卷积层步距是1。
+    但在pytorch官方实现过程中是第一个1x1卷积层的步距是1，第二个3x3卷积层步距是2，
+    这么做的好处是能够在top1上提升大概0.5%的准确率。
+    可参考Resnet v1.5 https://ngc.nvidia.com/catalog/model-scripts/nvidia:resnet_50_v1_5_for_pytorch
+    """
     expansion = 4
 
     def __init__(self, in_channel, out_channel, stride=1, downsample=None,
@@ -63,15 +57,11 @@ class Bottleneck(nn.Module):
                                kernel_size=3, stride=stride, bias=False, padding=1)
         self.bn2 = nn.BatchNorm2d(width)
         # -----------------------------------------
-        self.conv3 = nn.Conv2d(in_channels=width, out_channels=out_channel * self.expansion,
+        self.conv3 = nn.Conv2d(in_channels=width, out_channels=out_channel*self.expansion,
                                kernel_size=1, stride=1, bias=False)  # unsqueeze channels
-        self.bn3 = nn.BatchNorm2d(out_channel * self.expansion)
+        self.bn3 = nn.BatchNorm2d(out_channel*self.expansion)
         self.relu = nn.ReLU(inplace=True)
-
         self.downsample = downsample
-
-        self.stride = stride
-        self.ca = CoordAttention(in_channels=out_channel * self.expansion, out_channels=out_channel * self.expansion)
 
     def forward(self, x):
         identity = x
@@ -89,73 +79,109 @@ class Bottleneck(nn.Module):
         out = self.conv3(out)
         out = self.bn3(out)
 
-        out = self.ca(out)  # add CA
-
         out += identity
         out = self.relu(out)
 
         return out
 
+# Model Setting
+class BasicConv(nn.Module):
+    def __init__(self, in_planes, out_planes, kernel_size, stride=1, padding=0, dilation=1, groups=1, relu=True, bn=True, bias=False):
+        super(BasicConv, self).__init__()
+        self.out_channels = out_planes
+        self.conv = nn.Conv2d(in_channels=in_planes, out_channels=out_planes, kernel_size=kernel_size,
+                                stride=stride, padding=padding, dilation=dilation, groups=groups, bias=bias)
+        self.bn = nn.BatchNorm2d(out_planes, momentum=0.01, affine=True) if bn else None
+        self.relu = nn.ReLU() if relu else None
 
-class CANet(nn.Module):
-    def __init__(self, config):
-        super().__init__()
+    def forward(self, x):
+        x = self.conv(x)
+        if self.bn is not None:
+            x = self.bn(x)
+        if self.relu is not None:
+            x = self.relu(x)
+        return x
+class ResNet(nn.Module):
+
+    def __init__(self,
+                 config,
+                 block,
+                 blocks_num,
+                 groups=1,
+                 width_per_group=64,
+                 ):
+        super(ResNet, self).__init__()
+        #-----------------初始参数
         self.config = config
         bits = self.config.code_length
-        classlen = self.config.classlen
+        num_classes = self.config.classlen
 
-        if self.config.model_name == 'resnet50':
-            self.backbone = torchvision.models.resnet50(pretrained=True)
-            self.backbone.avgpool = nn.AdaptiveAvgPool2d(output_size=(1, 1))
-            # Adaptive hyper
-            self.alpha1 = nn.Parameter(torch.ones(1) * 1, requires_grad=True)
-            self.alpha2 = nn.Parameter(torch.ones(1) * 1, requires_grad=True)
 
-            self.backbone.b1 = nn.Linear(1024, bits)
-            self.backbone.b2 = nn.Linear(1024, bits)
-            self.backbone.b3 = nn.Linear(1024, bits)
-            self.backbone.b_cat = nn.Linear(3072, bits)
+        #-----------------实验独有参数
+        # Adaptive hyper
+        self.alpha1 = nn.Parameter(torch.ones(1) * 1, requires_grad=True)
+        self.alpha2 = nn.Parameter(torch.ones(1) * 1, requires_grad=True)
 
-            self.backbone.fc = nn.Linear(bits, classlen)
+        self.b1 = nn.Linear(1024, bits)
+        self.b2 = nn.Linear(1024, bits)
+        self.b3 = nn.Linear(1024, bits)
+        self.b_cat = nn.Linear(3072, bits)
 
-            self.relu = nn.ReLU(inplace=True)
-            self.num_ftrs = 2048
-            self.feature_size = 512
-            self.backbone.fc_x = nn.Linear(self.num_ftrs, classlen)
+        self.num_ftrs = 2048
+        self.feature_size = 512
+        self.fc_x = nn.Linear(self.num_ftrs, num_classes)
 
-            self.backbone.layer1 = self._make_layer(Bottleneck, 64, 3)
-            self.backbone.layer2 = self._make_layer(Bottleneck, 128, 4, stride=2)
-            self.backbone.layer3 = self._make_layer(Bottleneck, 256, 6, stride=2)
-            self.backbone.layer4 = self._make_layer(Bottleneck, 512, 3, stride=2)
-            # stage 1
-            self.backbone.conv_block1 = nn.Sequential(
-                BasicConv(self.num_ftrs // 4, self.feature_size, kernel_size=1, stride=1, padding=0, relu=True),
-                BasicConv(self.feature_size, self.num_ftrs // 2, kernel_size=3, stride=1, padding=1, relu=True),
-                nn.AdaptiveMaxPool2d(1),
-            )
+        # stage 1
+        self.conv_block1 = nn.Sequential(
+            BasicConv(self.num_ftrs // 4, self.feature_size, kernel_size=1, stride=1, padding=0, relu=True),
+            BasicConv(self.feature_size, self.num_ftrs // 2, kernel_size=3, stride=1, padding=1, relu=True),
+            nn.AdaptiveMaxPool2d(1),
+        )
 
-            # stage 2
-            self.backbone.conv_block2 = nn.Sequential(
-                BasicConv(self.num_ftrs // 2, self.feature_size, kernel_size=1, stride=1, padding=0, relu=True),
-                BasicConv(self.feature_size, self.num_ftrs // 2, kernel_size=3, stride=1, padding=1, relu=True),
-                nn.AdaptiveMaxPool2d(1),
-            )
+        # stage 2
+        self.conv_block2 = nn.Sequential(
+            BasicConv(self.num_ftrs // 2, self.feature_size, kernel_size=1, stride=1, padding=0, relu=True),
+            BasicConv(self.feature_size, self.num_ftrs // 2, kernel_size=3, stride=1, padding=1, relu=True),
+            nn.AdaptiveMaxPool2d(1),
+        )
 
-            # stage 3
-            self.backbone.conv_block3 = nn.Sequential(
-                BasicConv(self.num_ftrs, self.feature_size, kernel_size=1, stride=1, padding=0, relu=True),
-                BasicConv(self.feature_size, self.num_ftrs // 2, kernel_size=3, stride=1, padding=1, relu=True),
-                nn.AdaptiveMaxPool2d(1),
-            )
+        # stage 3
+        self.conv_block3 = nn.Sequential(
+            BasicConv(self.num_ftrs, self.feature_size, kernel_size=1, stride=1, padding=0, relu=True),
+            BasicConv(self.feature_size, self.num_ftrs // 2, kernel_size=3, stride=1, padding=1, relu=True),
+            nn.AdaptiveMaxPool2d(1),
+        )
 
-            # concat features from different stages
-            self.backbone.hashing_concat = nn.Sequential(
-                nn.BatchNorm1d(self.num_ftrs // 2 * 3, affine=True),
-                nn.Linear(self.num_ftrs // 2 * 3, self.feature_size),
-                nn.BatchNorm1d(self.feature_size, affine=True),
-                nn.ELU(inplace=True),
-                nn.Linear(self.feature_size, bits),
-            )
+        # concat features from different stages
+        self.hashing_concat = nn.Sequential(
+            nn.BatchNorm1d(self.num_ftrs // 2 * 3, affine=True),
+            nn.Linear(self.num_ftrs // 2 * 3, self.feature_size),
+            nn.BatchNorm1d(self.feature_size, affine=True),
+            nn.ELU(inplace=True),
+            nn.Linear(self.feature_size, bits),
+        )
+        #-----------------原始resnet结构
+        self.in_channel = 64
+
+        self.groups = groups
+        self.width_per_group = width_per_group
+
+        self.conv1 = nn.Conv2d(3, self.in_channel, kernel_size=7, stride=2,
+                               padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(self.in_channel)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, blocks_num[0])
+        self.layer2 = self._make_layer(block, 128, blocks_num[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, blocks_num[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, blocks_num[3], stride=2)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))  # output size = (1, 1)
+        self.fc = nn.Linear(bits, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
 
     def _make_layer(self, block, channel, block_num, stride=1):
         downsample = None
@@ -180,56 +206,50 @@ class CANet(nn.Module):
                                 width_per_group=self.width_per_group))
 
         return nn.Sequential(*layers)
+
     def forward(self, x):
-        return self.forward_vanilla(x)
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x1 = self.maxpool(x)
 
-    def forward_vanilla(self, x):
-        x = self.backbone.conv1(x)
-        x = self.backbone.bn1(x)
-        x = self.backbone.relu(x)
-        x1 = self.backbone.maxpool(x)
-        x2 = self.backbone.layer1(x1)
-        f1 = self.backbone.layer2(x2)
-        f2 = self.backbone.layer3(f1)
-        f3 = self.backbone.layer4(f2)
-
+        x2 = self.layer1(x1)
+        f1 = self.layer2(x2)
+        f2 = self.layer3(f1)
+        f3 = self.layer4(f2)
         feats = f3
 
-        f11 = self.backbone.conv_block1(f1).view(-1, self.num_ftrs // 2)
-        f11_b = self.backbone.b1(f11)
+        f11 = self.conv_block1(f1).view(-1, self.num_ftrs // 2)
+        f11_b = self.b1(f11)
 
-        f22 = self.backbone.conv_block2(f2).view(-1, self.num_ftrs // 2)
-        f22_b = self.backbone.b2(f22)
+        f22 = self.conv_block2(f2).view(-1, self.num_ftrs // 2)
+        f22_b = self.b2(f22)
 
-        f33 = self.backbone.conv_block3(f3).view(-1, self.num_ftrs // 2)
-        f33_b = self.backbone.b3(f33)
-        y33 = self.backbone.fc(f33_b)
+        f33 = self.conv_block3(f3).view(-1, self.num_ftrs // 2)
+        f33_b = self.b3(f33)
+        y33 = self.fc(f33_b)
 
         f44 = torch.cat((f11, f22, f33), -1)
-        f44_b = self.backbone.hashing_concat(f44)
+        f44_b = self.hashing_concat(f44)
 
-        # x = self.backbone.avgpool(feats)
+        # x = self.avgpool(feats)
         # x = torch.flatten(x, 1)
-        # y_x = self.backbone.fc_x(x)
+        # y_x = self.fc_x(x)
 
         return self.alpha1, self.alpha2, f44_b, y33, feats
 
 
+def resnet34(config):
+    # https://download.pytorch.org/models/resnet34-333f7ec4.pth
+    return ResNet(config, BasicBlock, [3, 4, 6, 3])
 
-# Model Setting
-class BasicConv(nn.Module):
-    def __init__(self, in_planes, out_planes, kernel_size, stride=1, padding=0, dilation=1, groups=1, relu=True, bn=True, bias=False):
-        super(BasicConv, self).__init__()
-        self.out_channels = out_planes
-        self.conv = nn.Conv2d(in_channels=in_planes, out_channels=out_planes, kernel_size=kernel_size,
-                                stride=stride, padding=padding, dilation=dilation, groups=groups, bias=bias)
-        self.bn = nn.BatchNorm2d(out_planes, momentum=0.01, affine=True) if bn else None
-        self.relu = nn.ReLU() if relu else None
 
-    def forward(self, x):
-        x = self.conv(x)
-        if self.bn is not None:
-            x = self.bn(x)
-        if self.relu is not None:
-            x = self.relu(x)
-        return x
+def resnet50(config):
+    # https://download.pytorch.org/models/resnet50-19c8e357.pth
+    return ResNet(config, Bottleneck, [3, 4, 6, 3])
+
+
+def resnet101(config):
+    # https://download.pytorch.org/models/resnet101-5d3b4d8f.pth
+    return ResNet(config, Bottleneck, [3, 4, 23, 3])
+
--- model/dct_model_interface.py
+++ model/dct_model_interface.py
@@ -3,7 +3,8 @@ from torch.nn import functional as F
 import pytorch_lightning as pl
 import os
 import torch_dct as DCT
-from model.ca_net_dct import CANet
+# from model.ca_net_dct import CANet
+from model.ca_resnet import resnet50
 from utils.attention_zoom import batch_augment
 from utils.evaluate import calc_map_k
 
@@ -11,7 +12,14 @@ class HInterface(pl.LightningModule):
     def __init__(self, config):
         super().__init__()
         self.config = config
-        self.model = CANet(self.config,isZoom=False)
+        self.model = resnet50(self.config)
+
+        model_weight_path = "./Model_result/model_EPA/ResNet50_EPA.pth"
+        assert os.path.exists(model_weight_path), "file {} does not exist.".format(model_weight_path)
+        self.model.load_state_dict(torch.load(model_weight_path))
+
+        for param in  self.model.parameters():
+            param.requires_grad = False
 
     def configure_optimizers(self):
         optimizer = torch.optim.SGD(self.parameters(), lr=self.config.lr, momentum=0.9, weight_decay=5e-4)
