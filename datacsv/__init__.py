b6d553b807b62f5420393debf032915783372cba
LiHuan <784353397@qq.com>
2024-03-16 10:21:50 +0800

    cc

--- datacsv/__init__.py
+++
@@ -1,4 +0,0 @@
-
-# from .data_interface import DInterface #原始
-from .dct_interface import DInterface #DCT实验
-# from .tfff_interface import DInterface #tfff实验
--- model/__init__.py
+++
@@ -1,3 +0,0 @@
-from .dct_model_interface import HInterface #dct
-# from .tfff_model_interface import HInterface #tfff
-# from .model_interface import HInterface #原始
